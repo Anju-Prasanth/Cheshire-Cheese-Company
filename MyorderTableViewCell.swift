f4c693f96fedde90cc295e39085e7c8c4b5c7b78
apple <apple@apples-Mac-mini.local>
2019-07-30 12:51:32 +0530

    first commit

---
+++ MyorderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyorderTableViewCell.swift
+//  
+//
+//  Created by apple on 20/04/19.
+//
+
+import UIKit
+
+class MyorderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
